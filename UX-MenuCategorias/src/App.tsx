a33d0c09423d1af1106261bd9186aa93d8fe2cae
Beatriz-Riby4320 <beatriz29851@estudante.ifb.edu.br>
2025-08-27 10:24:20 -0300

    rascunho inicial

---
+++ UX-MenuCategorias/src/App.tsx
@@ -0,0 +1,21 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <nav>
+        <ul className="menu">
+          <li><a href="#">Início</a></li>
+          <li><a href="#">Produtos</a></li>
+          <li><a href="#">Contato</a></li>
+        </ul>
+      </nav>
+      <main>
+        <h1>Bem-vindo ao nosso site!</h1>
+        <p>Escolha uma opção no menu acima.</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
